afbc87feefb451221c6bc4655f1d3b1d60f337d5
veetors <veetor.s@gmail.com>
2019-01-28 10:22:59 +0300

    add commander

--- src/bin/page-loader.js
+++ src/bin/page-loader.js
@@ -1,3 +1,16 @@
 #!/usr/bin/env node
 
-console.log('Welcome to page-loader');
+import program from 'commander';
+import pageLoader from '..';
+
+program
+  .version('0.0.1')
+  .description('Save web-page to output directory')
+  .option('--output [dir]', 'output directory (current by default)')
+  .arguments('<url>')
+  .action((url) => {
+    const dirPath = program.output || process.cwd();
+
+    pageLoader(url, dirPath);
+  })
+  .parse(process.argv);
---
+++ src/index.js
@@ -0,0 +1,3 @@
+export default (url, dirPath) => {
+  console.log(url, dirPath);
+};
